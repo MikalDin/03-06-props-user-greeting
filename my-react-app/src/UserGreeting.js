da7ddf32f2632689e5c181badb4d74f90d6ed2a3
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2023-12-12 06:21:56 -0500

    personalized experience

---
+++ my-react-app/src/UserGreeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserGreeting({ username }) {
+        return <p>Hello, { username }! glad you made it girl! WE have alot of fun activites planned for you.</p>
+}
+
+export default UserGreeting
